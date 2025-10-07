58ddfc18aab09a08ebdbe5b2297affd054bd81eb
Quphoria <s.simpson41.ss@googlemail.com>
2025-10-07 18:28:31 +0000

    Remove old backend

--- backend/healthcheck.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-    try:
-        with open("health.status") as f:
-            n = int(f.read())
-            sys.exit(n)
-    except Exception:
-        sys.exit(1) # unhealthy
